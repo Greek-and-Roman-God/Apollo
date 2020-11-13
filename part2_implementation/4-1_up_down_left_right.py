b97930e8ed8b35ad2c2401f48a11c045597697de
Doyeon <doyeon311@gmail.com>
2020-11-14 00:25:55 +0900

    구현 예제4-1 상하좌우 수정

--- part2_implementation/4-1_up_down_left_right.py
+++ part2_implementation/4-1_up_down_left_right.py
@@ -1,17 +1,32 @@
+# 싱히좌우
 space = int(input())
 plans = input().split(' ')
 x,y = [1,1]
 
-# direction = [{'U':+1},{'D':-1},{'L':-1},{'R':+1}]
-# print(direction)
+# 1
 for plan in plans:
     if plan == 'U' and y > 1: 
         y = y - 1
+        continue
     if plan == 'D' and y < space: 
         y = y + 1
+        continue
     if plan == 'R' and x < space: 
         x = x + 1
+        continue
     if plan == 'L' and x > 1: 
+        x = x - 1
+        continue
+
+# 2
+for plan in plans:
+    if plan == 'U' and y > 1: 
+        y = y - 1
+    elif plan == 'D' and y < space: 
+        y = y + 1
+    elif plan == 'R' and x < space: 
+        x = x + 1
+    elif plan == 'L' and x > 1: 
         x = x - 1
 print([y,x])
 
