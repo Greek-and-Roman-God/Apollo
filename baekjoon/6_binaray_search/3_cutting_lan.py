005722ccdaf35b0a454bb0399c2e28e7a64f818e
doyeon <doyeon311@gmail.com>
2020-11-19 01:06:39 +0900

    백준 이진탐색 정리

--- baekjoon/6_binaray_search/3_cutting_lan.py
+++ baekjoon/6_binaray_search/3_cutting_lan.py
@@ -1,1 +1,34 @@
 # 랜선 자르기
+def cutting_wire():
+    n, k = map(int, input().split())
+    wires = []
+    for _ in range(n):
+        wires.append(int(input()))
+
+    temp = min(wires)
+
+    while 1:
+        limit = 0
+        cnt = 0
+        for wire in wires:
+            cnt += wire // temp
+            print(f'cnt {cnt} temp {temp}')
+        print('------------------')
+        if cnt >= k:
+            temp = temp + 9
+            break
+        temp = temp - 10
+
+ 
+    for i in range(temp, temp-10,-1):
+        cnt = 0
+        
+        for wire in wires:
+            cnt += wire // i
+            print(f'out cnt {cnt} temp {i}')
+        print('------------------')
+
+        if cnt >= k:
+            print(i)
+            break
+cutting_wire()
--- baekjoon/6_binaray_search/4_cutting_tree.py
+++ baekjoon/6_binaray_search/4_cutting_tree.py
@@ -1,18 +1,17 @@
 # 나무 자르기
 from math import ceil
 
-n, need=map(int,input().split())
+n, need = map(int,input().split())
 tree_h = list(map(int,input().split()))
 
 tree_h.sort()
 tree_h.reverse()
 
-
 i = 0
 count = 1
 log = 0
 while 1:
-    log += (tree_h[i] - tree_h[i+1])*count
+    log += (tree_h[i] - tree_h[i+1]) * count
     if log >= need:
         # tree_h[i] > answer > tree_h[i+1]
         answer = tree_h[i] - ceil((log - need)/count) if log != need else tree_h[i+1]
