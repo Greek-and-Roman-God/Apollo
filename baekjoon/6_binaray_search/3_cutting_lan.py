2933740c8d28c15bc80fd7803144f5a643c49ef9
doyeon <doyeon311@gmail.com>
2020-11-19 03:30:25 +0900

    백준 이진탐색 랜선자르기 풀이 - 정리필요함

--- baekjoon/6_binaray_search/3_cutting_lan.py
+++ baekjoon/6_binaray_search/3_cutting_lan.py
@@ -5,30 +5,32 @@ def cutting_wire():
     for _ in range(n):
         wires.append(int(input()))
 
-    temp = min(wires)
+    temp = max(wires)
 
+    limit = 0
+    m_value = 0
+    m_cnt = 0
     while 1:
-        limit = 0
         cnt = 0
         for wire in wires:
             cnt += wire // temp
-            print(f'cnt {cnt} temp {temp}')
+            print(f'cnt {cnt} temp {temp} m_cnt {m_cnt}')
         print('------------------')
-        if cnt >= k:
-            temp = temp + 9
+        if cnt == m_cnt and temp == m_value:
+            print(temp)
             break
-        temp = temp - 10
-
- 
-    for i in range(temp, temp-10,-1):
-        cnt = 0
-        
-        for wire in wires:
-            cnt += wire // i
-            print(f'out cnt {cnt} temp {i}')
-        print('------------------')
 
         if cnt >= k:
-            print(i)
-            break
+            m_cnt = cnt
+            m_value = temp
+            temp = temp + temp//2
+        elif cnt < m_cnt:
+            temp = (m_value+temp)//2
+        else:
+            temp = temp//2
+
+        # limit+=1
+        # if limit > 100:
+        #     break
+
 cutting_wire()
