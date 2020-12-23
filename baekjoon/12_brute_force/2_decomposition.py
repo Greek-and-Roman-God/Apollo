8cac3c7f9c4edb07714c629729501cec5870d4ad
doyeon <doyeon311@gmail.com>
2020-12-23 15:36:44 +0900

    풀이: 백준 2231 분해합

---
+++ baekjoon/12_brute_force/2_decomposition.py
@@ -0,0 +1,30 @@
+# 분해합
+# https://www.acmicpc.net/problem/2231
+n = int(input())
+
+
+for number in range(1, n):
+    temp = [number]
+    if number > 10:
+        for num in str(number):
+            temp.append(int(num))
+
+    if sum(temp) == n:
+        print(number)
+        break
+else:
+    print(0)
+
+
+# 다른 사람 풀이
+n = input()
+num = int(n)
+start = max(num - 9*len(n), 0)
+res = []
+for i in range(start, num):
+    if num == sum(map(int, str(i)))+i:
+        res.append(i)
+if len(res) == 0:
+    print(0)
+else:
+    print(min(res))
