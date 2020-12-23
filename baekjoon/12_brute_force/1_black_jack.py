914931c9a0952fbe4b9157583f4ff49a65b638c2
doyeon <doyeon311@gmail.com>
2020-12-23 15:35:54 +0900

    풀이: 백준 2798 블랙잭

---
+++ baekjoon/12_brute_force/1_black_jack.py
@@ -0,0 +1,18 @@
+# 2798 블랙잭
+# https://www.acmicpc.net/problem/2798
+n, m = map(int, input().split())
+
+cards = list(map(int, input().split()))
+
+temp = []
+
+for i, c in enumerate(cards):
+    for j, a in enumerate(cards[i+1:]):
+        for r in cards[i+1+j+1:]:
+            print(f'c,a,r {c, a, r}')
+            num = c+a+r
+            if num > m:
+                continue
+            else:
+                temp.append(num)
+print(temp)
