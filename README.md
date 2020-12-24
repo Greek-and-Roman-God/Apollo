d9526ac14b2ff6531d0174874a53a083d67ed42e
doyeon <doyeon311@gmail.com>
2020-12-24 21:33:35 +0900

    풀이: 백준 1018 체스판 다시 칠하기

--- README.md
+++ README.md
@@ -205,5 +205,5 @@
 - [x] 블랙잭[📌](https://github.com/Greek-and-Roman-God/Apollo/blob/main/baekjoon/12_brute_force/1_black_jack.py)
 - [x] 분해합[📌](https://github.com/Greek-and-Roman-God/Apollo/blob/main/baekjoon/12_brute_force/2_decomposition.py)
 - [x] 덩치[📌](https://github.com/Greek-and-Roman-God/Apollo/blob/main/baekjoon/12_brute_force/3_bulk.py)
-- [ ] 체스판 다시 칠하기[](https://github.com/Greek-and-Roman-God/Apollo/blob/main/baekjoon/12_brute_force/.py)
+- [x] 체스판 다시 칠하기[📌](https://github.com/Greek-and-Roman-God/Apollo/blob/main/baekjoon/12_brute_force/4_repiainting_chess_board.py)
 - [x] 영화감독 숍[📌](https://github.com/Greek-and-Roman-God/Apollo/blob/main/baekjoon/12_brute_force/5_director_shop.py)
---
+++ baekjoon/12_brute_force/4_repiainting_chess_board.py
@@ -0,0 +1,70 @@
+# 체스판 다시 칠하기
+# https://www.acmicpc.net/problem/1018
+
+# n, m = 8, 8
+# board = [
+#     'WBWBWBWB',
+#     'BWBWBWBW',
+#     'WBWBWBWB',
+#     'BWBBBWBW',
+#     'WBWBWBWB',
+#     'BWBWBWBW',
+#     'WBWBWBWB',
+#     'BWBWBWBW',
+# ]
+
+# n, m = 10, 13
+# board = [
+
+#     'BBBBBBBBWBWBW',
+#     'BBBBBBBBBWBWB',
+#     'BBBBBBBBWBWBW',
+#     'BBBBBBBBBWBWB',
+#     'BBBBBBBBWBWBW',
+#     'BBBBBBBBBWBWB',
+#     'BBBBBBBBWBWBW',
+#     'BBBBBBBBBWBWB',
+#     'WWWWWWWWWWBWB',
+#     'WWWWWWWWWWBWB',
+# ]
+
+temp_board = [
+    ['WBWBWBWB',
+     'BWBWBWBW',
+     'WBWBWBWB',
+     'BWBWBWBW',
+     'WBWBWBWB',
+     'BWBWBWBW',
+     'WBWBWBWB',
+     'BWBWBWBW'],
+    ['BWBWBWBW',
+     'WBWBWBWB',
+     'BWBWBWBW',
+     'WBWBWBWB',
+     'BWBWBWBW',
+     'WBWBWBWB',
+     'BWBWBWBW',
+     'WBWBWBWB']
+]
+n, m = map(int, input().split())
+board = []
+for _ in range(n):
+    row = input()
+    board.append(row)
+
+temp = 33
+for i in range(n-8+1):
+    for j in range(m-8+1):
+        for t_board in temp_board:
+            paint = 0
+            for row, temp_row in zip(board[i:i+8], t_board):
+                row = row[j:j+8]
+                # print(f'{row} {temp}')
+                for color, temp_color in zip(row, temp_row):
+                    if color != temp_color:
+                        paint += 1
+            else:
+                # print(f"{paint}-------------------")
+                temp = paint if temp > paint else temp
+
+print(temp)
