bda0c482eb422e2e71691d09d23d1856c29b55cf
doyeon <doyeon311@gmail.com>
2020-12-28 18:21:34 +0900

    풀이: 백준 2751 풀이 추가

--- baekjoon/13_sort/2_sorting_number2.py
+++ baekjoon/13_sort/2_sorting_number2.py
@@ -19,3 +19,16 @@ def heap_sort(a):
 
 
 heap_sort(nums)
+
+
+# 2
+
+n = int(input())
+nums = []
+for _ in range(n):
+    num = int(input())
+    nums.append(num)
+
+nums.sort()
+for num in nums:
+    print(num)
