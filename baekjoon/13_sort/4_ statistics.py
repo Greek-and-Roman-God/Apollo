b80be06dec162de2ba42cdb3e23672fec895a7fb
doyeon <doyeon311@gmail.com>
2020-12-30 01:16:25 +0900

    풀이: 백준 2108 통계학

---
+++ baekjoon/13_sort/4_ statistics.py
@@ -0,0 +1,38 @@
+# 2108 통계학
+# https://www.acmicpc.net/problem/2108
+n = int(input())
+
+scores = []
+cnt = {}
+max_cnt = 0  # 최빈값의 횟수
+for _ in range(n):
+    score = int(input())
+    scores.append(score)
+    if score in cnt:
+        cnt[score] += 1
+    else:
+        cnt[score] = 1
+    if max_cnt < cnt[score]:
+        max_cnt = cnt[score]
+scores.sort()
+
+
+# 산술평균
+print(int(round(sum(scores) / float(n), 0)))
+# 중앙 값
+print(scores[n//2])
+# 최빈값
+c = 0
+freq = []
+for key in cnt:
+    if cnt[key] == max_cnt:
+        freq.append(int(key))
+freq = list(set(freq))
+if len(freq) > 1:
+    freq.sort()
+    print(freq[1])
+else:
+    print(freq[0])
+
+# 범위
+print(scores[n-1] - scores[0])
