5db2a23ab3236d114547f9a77525bef1dc3f6ffb
doyeon <doyeon311@gmail.com>
2020-12-30 22:00:55 +0900

    풀이: 백준 2798 블랙잭 풀이 추가

--- baekjoon/12_brute_force/1_black_jack.py
+++ baekjoon/12_brute_force/1_black_jack.py
@@ -16,3 +16,23 @@ for i, c in enumerate(cards):
             else:
                 temp.append(num)
 print(max(temp))
+
+
+# 2
+n, m = map(int, input().split())
+
+cards = list(map(int, input().split()))
+
+temp = 0
+
+for i, c in enumerate(cards):
+    for j, a in enumerate(cards[i+1:]):
+        for r in cards[i+1+j+1:]:
+            print(f'c,a,r {c, a, r}')
+            num = c+a+r
+            if num > m:
+                continue
+            else:
+                if temp < num:
+                    temp = num
+print(temp)
