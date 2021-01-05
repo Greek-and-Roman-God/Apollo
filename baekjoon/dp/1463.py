62ddb053fba3076345e32a41aecae0d8ede697f4
doyeon <doyeon311@gmail.com>
2021-01-05 15:45:13 +0900

    풀이: 벡준 1로 만들기

---
+++ baekjoon/dp/1463.py
@@ -0,0 +1,14 @@
+# 1463 1로 만들기
+# https://www.acmicpc.net/problem/1463
+n = int(input())
+
+dp = [0 for _ in range(n+1)]
+cnt = 0
+
+for i in range(2, n+1):
+    dp[i] = dp[i-1] + 1
+    if i % 2 == 0 and dp[i] > dp[i//2]+1:
+        dp[i] = dp[i//2] + 1
+    if i % 3 == 0 and dp[i] > dp[i//3]+1:
+        dp[i] = dp[i//3] + 1
+print(dp[n])
