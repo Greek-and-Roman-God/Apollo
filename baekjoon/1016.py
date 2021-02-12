23e2d194ff36d3c836039b4159c0d1b851d693cf
doyeon <doyeon311@gmail.com>
2021-02-12 23:51:37 +0900

    정리: 백준 제곱 ㄴㄴ수

--- baekjoon/1016.py
+++ baekjoon/1016.py
@@ -1,34 +1,29 @@
 # 1016 제곱 ㄴㄴ 수
 # https://www.acmicpc.net/problem/1016
 
-from math import sqrt
-min, max = map(int, input().split())
-# print(sqrt(min), sqrt(max))
-temp_min = int(sqrt(min))
-temp_max = int(sqrt(max))
+# 다시 풀어보기
+import sys
+import math
 
-temp = 0
+results = []
+min_num, max_num = map(int, sys.stdin.readline().split())
+validation = [1 for _ in range(min_num, max_num+1)]
 
-for i in range(temp_min, temp_max+1):
-    if i**2 <= max:
-        temp += 1
-# print(temp)
-# print(max-min+1)
-print(max-min+1-temp)
-# n = 1000
-# mat = [0] * (n+1)
-# mat[1] = 1
+search_target = int(math.sqrt(max_num))
+# max의 값보다 작은 모든 제곱수의 목록을 생성
+squares = [v**2 for v in range(2, search_target+1)]
+for square in squares:
+    # min부터 max까지의 수 중, 해당 제곱수의 최소의 배수를 구함.
+    cur_idx = (math.ceil(min_num / square) * square) - min_num
+    while cur_idx <= max_num - min_num:
+        # 제곱수의 배수인 경우 0으로 대체
+        validation[cur_idx] = 0
+        # 다음 제곱수의 index를 구함.
+        cur_idx += square
 
-# for i in range(2, n):
-#     if i ** 2 <= n and mat[i**2] == 0:
-#         mat[i**2] = 1
+# 남은 1들의 개수가 제곱 ㄴㄴ수의 개수가 된다.
+result = sum(validation)
+results.append(result)
 
-# nums = []
-
-# for num, m in enumerate(mat):
-#     if m == 1:
-#         nums.append(num)
-# print(nums)
-
-# # for num in range(min, max+1):
-# #     for nono in nums:
+for result in results:
+    sys.stdout.write(str(result))
