f0fe2b608adfd2943f4d40bfe066e5c189a97969
doyeon <doyeon311@gmail.com>
2021-03-15 21:00:54 +0900

    풀이: 백준 최대힙

---
+++ baekjoon/2021-03-14/11279.py
@@ -0,0 +1,21 @@
+# 최대 힙
+# https://www.acmicpc.net/problem/11279
+import heapq
+import sys
+
+
+def input():
+    return sys.stdin.readline()
+
+
+n = int(input())
+heap = []
+for _ in range(n):
+    x = int(input())
+    if x:
+        heapq.heappush(heap, (-x, x))
+    elif len(heap):
+        num = heapq.heappop(heap)
+        print(num[1])
+    else:
+        print(0)
