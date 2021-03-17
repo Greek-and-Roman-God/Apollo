8331bb5e857710b04d4e278fb1b8cb6467597c64
doyeon <doyeon311@gmail.com>
2021-03-17 23:32:02 +0900

    풀이: 백준 포도주 시식

--- baekjoon/2021-02-21/2156.py
+++ baekjoon/2021-02-21/2156.py
@@ -11,8 +11,7 @@ if n > 1:
     dp[2] = wines[1] + wines[2]
 
 for i in range(3, n+1):
-    dp[i] = max(wines[i-1]+dp[i-3]+wines[i],
-                dp[i-2]+wines[i], dp[i-1])
+    dp[i] = max(wines[i-1]+dp[i-3]+wines[i], dp[i-2]+wines[i], dp[i-1])
 
 print(dp[n])
 # print(dp)
