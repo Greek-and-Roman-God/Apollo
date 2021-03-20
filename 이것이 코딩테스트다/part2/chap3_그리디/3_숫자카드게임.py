1e29f1fbf3b7db72224c7b9b8f965d811f63f9d7
doyeon <doyeon311@gmail.com>
2021-03-21 01:44:18 +0900

    풀이: 이코테 숫자카드게임

---
+++ 이것이 코딩테스트다/part2/chap3_그리디/3_숫자카드게임.py
@@ -0,0 +1,20 @@
+# 숫자 카드 게임
+
+n, m = map(int, input().split())
+
+cards = [min(list(map(int, input().split()))) for _ in range(n)]
+print(max(cards))
+
+# 2
+result = 0
+
+for i in range(n):
+    data = list(map(int, input().split()))
+    min_value = 10001
+
+    for d in data:
+        min_value = min(min_value, d)
+
+    result = max(result, min_value)
+
+print(result)
