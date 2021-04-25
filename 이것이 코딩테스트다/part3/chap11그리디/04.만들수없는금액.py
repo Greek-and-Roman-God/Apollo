eb1bd063269378e078b9579a1accdb94a08393a6
doyeon <doyeon311@gmail.com>
2021-04-25 21:11:35 +0900

    풀이: 이코테 그리디 만들 수 없는 금액

---
+++ 이것이 코딩테스트다/part3/chap11그리디/04.만들수없는금액.py
@@ -0,0 +1,13 @@
+# 만들 수 없는 금액
+n = int(input())
+coins = list(map(int, input().split()))
+coins.sort()
+target = 1
+
+for coin in coins:
+    if coin <= target:
+        target += coin
+    else:
+        break
+
+print(target)
