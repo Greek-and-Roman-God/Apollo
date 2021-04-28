5dda7c71ea93561bc7583690715628c4ef2fd463
doyeon <doyeon311@gmail.com>
2021-04-28 23:11:27 +0900

    수정: 이코테 풀이 수정

--- 이것이 코딩테스트다/part2/chap8_다이나믹프로그래밍/4_바닥공사.py
+++ 이것이 코딩테스트다/part2/chap8_다이나믹프로그래밍/4_바닥공사.py
@@ -5,5 +5,5 @@ dp[0] = 1
 dp[1] = 1
 dp[2] = 3
 for i in range(3, n+1):
-    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+    dp[i] = (dp[i-1] + dp[i-2] + dp[i-2]) % 796796
 print(dp[n])
--- 이것이 코딩테스트다/part2/chap8_다이나믹프로그래밍/5_효율적인화폐구성.py
+++ 이것이 코딩테스트다/part2/chap8_다이나믹프로그래밍/5_효율적인화폐구성.py
@@ -7,8 +7,15 @@ for _ in range(n):
 dp = [10001] * (m+1)
 dp[0] = 0
 
-for i in range(1, m+1):
-    for coin in coins:
-        if i-coin >= 0:
-            dp[i] = min(dp[i], dp[i-coin]+1)
-print(dp[m] if dp[m] == 10001 else -1)
+# for i in range(1, m+1):
+#     for coin in coins:
+#         if i-coin >= 0:
+#             dp[i] = min(dp[i], dp[i-coin]+1)
+
+for coin in coins:
+    for j in range(coin, m+1):
+        if dp[j-coin] != 10001:
+            dp[j] = min(dp[j], dp[j-coin] + 1)
+
+print(dp[:m+1])
+print(dp[m] if dp[m] != 10001 else -1)
