04bb1756e5fd8e4682e333c6adadf618f8fa77ef
doyeon <doyeon311@gmail.com>
2021-05-30 21:44:01 +0900

    정리 : 백준 KCM Travel 다시 검색해보고 공부

--- baekjoon/graph/10217.py
+++ baekjoon/graph/10217.py
@@ -1,6 +1,9 @@
 # KCM Travel
 # https://www.acmicpc.net/problem/10217
-import heapq
+# 소요시간은 짧지만 비용이 비싼 경우와 소요시간은 길지만 비용이 싼 경우를 모두 탐색해야한다.
+# 소요시간이 짧은 경로를 탐색하는 다익스트라만 사용할 경우 원하는 결과를 얻지 못할 수 있다.
+# 그래서 사용하는 것이 DP
+
 import sys
 
 input = sys.stdin.readline
@@ -17,8 +20,10 @@ for _ in range(t):
         u, v, c, d = map(int, input().split())
         graph[u].append((v, c, d))
 
+    # 주어진 정점 * 총 지원 비용 크기의 배열을 생성하고 INF로 초기화
+    # 모든 정점에 대해 총 지원 비용 이내에 해당하는 모든 금액으로 탐색을 진행한다는 의미
     dp = [[INF] * (m+1) for _ in range(n+1)]
-    dp[1][0] = 0
+    dp[1][0] = 0    # 출발지점 1번공항, 1번 공항에 대한 비용은 0원
 
     for money in range(m+1):
         for now in range(1, n+1):
@@ -29,8 +34,10 @@ for _ in range(t):
                 if money + next_m > m:
                     continue
 
-                dp[next_port][money +
-                              next_m] = min(dp[next_port][money+next_m], now_dist + next_d)
+                # next_port 공항의 money + next_m의 비용으로 갈 수 있는 시간을 최소로 업데이트
+                dp[next_port][money + next_m] =\
+                    min(dp[next_port][money+next_m], now_dist + next_d)
+    # n공항에 저장된 소요시간 중 가장 빠른 시간을 선택
     result = min(dp[n])
     if result != INF:
         print(result)
