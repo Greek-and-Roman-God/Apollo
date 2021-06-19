4796b8109b2d3ca8fe0d3d87871fa97515752195
doyeon <doyeon311@gmail.com>
2021-06-19 18:51:16 +0900

    풀이: 이코테 최단경로 정확한 순위

---
+++ 이것이 코딩테스트다/part3/chap17_최단경로/38.정확한순위.py
@@ -0,0 +1,31 @@
+# 정확한 순위
+INF = int(10e9)
+
+n, m = map(int, input().split())
+
+graph = [[INF]*(n+1) for _ in range(n+1)]
+
+for i in range(1, n+1):
+    graph[i][i] = 0
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+
+for k in range(1, n + 1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
+
+result = 0
+
+for i in range(1, n+1):
+    count = 0
+    for j in range(1, n+1):
+        if graph[i][j] != INF or graph[j][i] != INF:
+            count += 1
+    if count == n:
+        result += 1
+for g in graph:
+    print(g)
+print(result)
