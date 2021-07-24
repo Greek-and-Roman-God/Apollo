80c79440be982194108c43efcce8750c39e5d1c9
doyeon <doyeon311@gmail.com>
2021-07-24 23:00:27 +0900

    풀이: 이코테 그리드 1일 될 때까지

--- 이것이 코딩테스트다/part2/chap3_그리디/4_1이 될 때까지.py
+++ 이것이 코딩테스트다/part2/chap3_그리디/4_1이 될 때까지.py
@@ -1,15 +1,26 @@
 import math
 # 1이 될 때까지
+# n, k = map(int, input().split())
+
+# temp = n
+# cnt = 0
+# while True:
+#     temp //= k
+
+#     if temp == 0:
+#         break
+#     cnt += 1
+
+# result = cnt + n % k
+# print(result)
+
+
+# 2021-07-25
 n, k = map(int, input().split())
+result = 0
+while n != 1 and n > k:
+    result += n % k + 1  # 1씩 뺄 횟수(나머지) + k로 나눈 횟수(1)
+    n = n // k
 
-temp = n
-cnt = 0
-while True:
-    temp //= k
-
-    if temp == 0:
-        break
-    cnt += 1
-
-result = cnt + n % k
+result += n - 1  # 마지막 남은 k보다 작은 값은 따로 횟수를 더해준다.
 print(result)
