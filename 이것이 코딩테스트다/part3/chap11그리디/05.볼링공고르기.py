8737b7277610f67682d1f6ce6e92a8f947984749
doyeon <doyeon311@gmail.com>
2021-07-25 00:51:01 +0900

    풀이: 이코테 그리디 볼링공고르기

--- 이것이 코딩테스트다/part3/chap11그리디/05.볼링공고르기.py
+++ 이것이 코딩테스트다/part3/chap11그리디/05.볼링공고르기.py
@@ -1,10 +1,21 @@
 # 볼링공 고르기
-m, n = map(int, input().split())
-balls = list(map(int, input().split()))
-dupl = m - len(list(set(balls)))
-print(dupl)
-print(int(m*(m-1)/2 - int(dupl*(dupl-1))))
+# m, n = map(int, input().split())
+# balls = list(map(int, input().split()))
+# dupl = m - len(list(set(balls)))
+# print(dupl)
+# print(int(m*(m-1)/2 - int(dupl*(dupl-1))))
 
 # 5 3
 # 1 2 3 2 3
 # 5 * 4/2 - (5-3)
+
+# 2021-07-25
+n, m = map(int, input().split())
+balls = list(map(int, input().split()))
+result = n * (n-1) // 2
+
+for ball in set(balls):
+    count = balls.count(ball)
+    result -= count * (count - 1) // 2
+
+print(result)
