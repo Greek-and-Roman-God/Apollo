bb734b7431e6a0860bfff6d497a45e1162a0b103
doyeon <doyeon311@gmail.com>
2021-07-24 23:00:51 +0900

    풀이: 이코테 그리드 큰수의 법칙

--- 이것이 코딩테스트다/part2/chap3_그리디/2_큰수의법칙.py
+++ 이것이 코딩테스트다/part2/chap3_그리디/2_큰수의법칙.py
@@ -26,3 +26,20 @@ count = int(m2/(k+1)) * k + m2 % (k+1)
 
 result = count * num1 + (m2-count) * num2
 print(result)
+
+
+# 2021-07-18
+n, m, k = map(int, input().split())
+nums = list(map(int, input().split()))
+
+nums.sort()
+
+num1 = nums[-1]
+num2 = nums[-2]
+
+repeat = m // (k+1)
+rest = m % (k+1)
+
+print(repeat * num1 * k, rest * num1, repeat * num2)
+
+result = repeat * num1 * k + rest * num1 + repeat * num2
