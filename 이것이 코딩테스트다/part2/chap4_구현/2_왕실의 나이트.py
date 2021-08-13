f0137321075d034e2b7e47caaf9c59f03f1b469f
Kim Doyeon <53068706+kimdoyeonn@users.noreply.github.com>
2021-08-13 10:31:35 +0900

    풀이: 이코테 구현 왕실의 나이트

--- 이것이 코딩테스트다/part2/chap4_구현/2_왕실의 나이트.py
+++ 이것이 코딩테스트다/part2/chap4_구현/2_왕실의 나이트.py
@@ -13,3 +13,20 @@ for move in moves:
     if 0 < next_x < 9 and 0 < next_y < 9:
         result += 1
 print(result)
+
+
+# 2021-08-13
+x, y = list(input())
+# print(x,y)
+x = ord(x) - ord('a')
+y = ord(y) - ord('1')
+
+moves = [(1, -2), (-1, -2),(1, 2), (-1, 2),(-2, 1), (-2, 1),(2, 1), (2, -1)]
+
+answer = 0
+for move in moves:
+  nx, ny = x+move[0], y + move[1]
+  if 0 <= nx < 8 and 0 <= ny < 8:
+    answer += 1
+
+print(answer)
