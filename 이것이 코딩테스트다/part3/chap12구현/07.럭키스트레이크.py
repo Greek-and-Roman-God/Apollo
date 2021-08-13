8a1b7fca5c9a529083c5990840a7f9c0f378d541
Kim Doyeon <53068706+kimdoyeonn@users.noreply.github.com>
2021-08-13 10:31:12 +0900

    풀이: 이코테 구현 럭키스트레이크

--- 이것이 코딩테스트다/part3/chap12구현/07.럭키스트레이크.py
+++ 이것이 코딩테스트다/part3/chap12구현/07.럭키스트레이크.py
@@ -13,3 +13,16 @@ if cal(score1) == cal(score2):
     print("LUCKY")
 else:
     print("READY")
+
+
+# 2021-08-13
+score = list(map(int, list(input()))
+
+score1 = score[:len(score) // 2]
+score2 = score[len(score) // 2:]
+
+if sum(score1) == sum(score2):
+    print("LUCKY")
+else:
+    print("READY")
+
