b80ea84bb4dda5ba72386bfa6ad4adf92f2aa4b6
doyeon <doyeon311@gmail.com>
2021-10-02 19:53:53 +0900

    풀이: 이코테 그래프이론 도시분할계획

--- 이것이 코딩테스트다/part2/chap10_그래프이론/3_도시분할계획.py
+++ 이것이 코딩테스트다/part2/chap10_그래프이론/3_도시분할계획.py
@@ -44,3 +44,41 @@ for edge in edges:
         last = cost
 
 print(result - last)
+
+# 2021-10-02
+
+n, m = map(int, input().split())
+parent = [0] * (n+1)
+
+edges = []
+result = 0
+
+for i in range(1, n+1):
+    parent[i] = i
+
+for _ in range(m):
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
+
+# 비용을 기준으로 간선을 정렬함
+# 마을을 두 개로 나누려면 간선 하나를 제거
+# 문제에서 비용의 합의 최솟값을 구하라고 했으므로
+# 가장 비싼 간선을 제거
+edges.sort()
+last = 0
+
+# 모든 집들을 연결함
+for edge in edges:
+    cost, a, b = edge
+    # a와 b 사이를 이동할 수 없다면
+    if find_parent(parent, a) != find_parent(parent, b):
+        # 도로를 연결
+        union_parent(parent, a, b)
+        # 도로의 비용을 저장
+        result += cost
+        # 가장 마지막에 도로의 비용(제일 비싼 도로)을 저장
+        last = cost
+    # 아니라면 패스
+
+# 모든 도로의 비용에서 가장 마지막에 연결된 도로의 비용을 제한게 결과
+print(result - last)
